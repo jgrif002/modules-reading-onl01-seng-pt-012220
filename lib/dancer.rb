4ffaeb91dc86b1b11403af507853a0d9b9cbc008
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-25 03:17:39 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,6 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  
-  include Dance
-  
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
